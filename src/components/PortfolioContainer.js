211a9680bac64fcc1d931cac21c432c30b9bf183
gitkcb <kcharlesbell@gmail.com>
2023-02-14 12:41:29 -0500

    basic file structure running

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import Footer from './Footer';
+// import Footer from './Footer';
 import Navigation from './Navigation';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
 
 export default function PortfolioContainer(){
-    const [currentPage, setCurrentPage] = useState('About');
 
-    const renderPage = () => {
-        if (currentPage === 'About') {
-            return <About />;
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />;
-        }
-        if (currentPage === 'Portfolio') {
-            return <Portfolio />;
-        }
-        if (currentPage === 'Resume') {
-            return <Resume />;
-        }
-    };
-    const handlePageChange = (page) => setCurrentPage(page);
+    const renderHeader = () => {
+        return <Header />;
+    }
+    renderHeader();
+    // const [currentPage, setCurrentPage] = useState('About');
 
-    return (
-      <div>
+    // const renderPage = () => {
+    //     if (currentPage === 'About') {
+    //         return <About />;
+    //     }
+    //     if (currentPage === 'Contact') {
+    //         return <Contact />;
+    //     }
+    //     if (currentPage === 'Portfolio') {
+    //         return <Portfolio />;
+    //     }
+    //     if (currentPage === 'Resume') {
+    //         return <Resume />;
+    //     }
+    // };
+    // const handlePageChange = (page) => setCurrentPage(page);
+
+    // return (
+    //   <div>
        
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-      </div>
-    );
-
+    //     <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+    //     {renderPage()}
+    //   </div>
+    // );
+ 
 }
