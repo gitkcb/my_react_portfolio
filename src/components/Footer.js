a5341a5946bef416ac14710e72ffcbca81ddf5d0
gitkcb <kcharlesbell@gmail.com>
2023-02-16 13:36:54 -0500

    added linkedin link and email to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,21 @@
 
 function Footer() {
     return(
-        <div>
-            Footer
-        </div>
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                        <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+                        <a href = "https://github.com/gitkcb" target="_blank" rel="noreferrer">
+                            Kevin Bell
+                        </a>
+                    <hr />
+                    <a href = "www.linkedin.com/in/kcharlesbell" target="_blank" rel="noreferrer"> LinkedIn</a>
+                    {" "}
+                    |{" "}{" "}
+                    <a href="mailto:kcharlesbell@gmail.com" target="_blank" rel="noreferrer">kcharlesbell@gmail.com</a>
+                </p>
+            </div>
+            </footer>
     )
 }
 export default Footer
